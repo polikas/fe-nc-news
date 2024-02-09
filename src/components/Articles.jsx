acdff3807388832309846b507079b619ec817cc0
stavros <polik_as@hotmail.com>
2024-02-09 11:22:15 +0000

    sort articles menu feature  added

--- src/components/Articles.jsx
+++ src/components/Articles.jsx
@@ -3,30 +3,59 @@ import { ArticleContext } from "../contexts/Article";
 import { getArticles } from "../api";
 import ArticleCard from "./ArticleCard";
 import { useParams } from "react-router-dom";
+import { Box, FormControl, InputLabel, MenuItem, Select } from "@mui/material";
 
 const Articles = () => {
   const { articles, setArticles } = useContext(ArticleContext);
   const [isLoading, setIsLoading] = useState(true);
+  const [sortBy, setSortBy] = useState("votes");
   const { slug } = useParams();
 
   useEffect(() => {
     setIsLoading(true);
     getArticles(slug).then((articlesData) => {
-      setArticles(articlesData.articles);
+      const sortArticles = [...articlesData.articles].sort(
+        (a, b) => b[sortBy] - a[sortBy]
+      );
+
+      setArticles(sortArticles);
       setIsLoading(false);
     });
-  }, []);
+  }, [sortBy]);
+
+  const handleChange = (event) => {
+    setSortBy(event.target.value);
+  };
 
   if (isLoading) {
     return <p>Loading...</p>;
   }
 
   return (
-    <ul>
-      {articles.map((article) => (
-        <ArticleCard key={article.article_id} article={article} />
-      ))}
-    </ul>
+    <>
+      <h2>Sort Articles</h2>
+      <Box>
+        <FormControl sx={{ m: 1, minWidth: 80 }}>
+          <InputLabel id="sort_by_articles">Date</InputLabel>
+          <Select
+            labelId="sort_by_articles"
+            id="sort_by"
+            value={sortBy}
+            label="Sort"
+            onChange={handleChange}
+          >
+            <MenuItem value={"created_at"}>Date</MenuItem>
+            <MenuItem value={"comment_count"}>Comment Count</MenuItem>
+            <MenuItem value={"votes"}>Votes</MenuItem>
+          </Select>
+        </FormControl>
+      </Box>
+      <ul>
+        {articles.map((article) => (
+          <ArticleCard key={article.article_id} article={article} />
+        ))}
+      </ul>
+    </>
   );
 };
 
