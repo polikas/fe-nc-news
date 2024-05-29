221d2c016d70edbe09767441912b3426b5f5da1c
stavros <polik_as@hotmail.com>
2024-05-29 05:33:44 +0100

    added footer to the page

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,21 +7,25 @@ import Comments from "./Comments";
 import SingleArticle from "./SingleArticle";
 import UserAccount from "./UserAccount";
 import TopicsList from "./TopicsList";
+import Footer from "./Footer";
 
 function App() {
   return (
-    <div>
-      <HomeHeader />
-      <Routes>
-        <Route path="/" element={<UserAccount />} />  
-        <Route path="/navigation" element={<Navigation />} />
-        <Route path="/articles" element={<Articles />} />
-        <Route path="/articles/:article_id" element={<SingleArticle />} />
-        <Route path="/articles/:article_id/comments" element={<Comments />} />
-        <Route path="/topics" element={<TopicsList />} />
-        <Route path="/articles/topic/:slug" element={<Articles />} />
-      </Routes>
-    </div>
+    <>
+      <div className="content-container">
+        <HomeHeader />
+        <Routes>
+          <Route path="/" element={<UserAccount />} />
+          <Route path="/navigation" element={<Navigation />} />
+          <Route path="/articles" element={<Articles />} />
+          <Route path="/articles/:article_id" element={<SingleArticle />} />
+          <Route path="/articles/:article_id/comments" element={<Comments />} />
+          <Route path="/topics" element={<TopicsList />} />
+          <Route path="/articles/topic/:slug" element={<Articles />} />
+        </Routes>
+      </div>
+      <Footer />
+    </>
   );
 }
 
